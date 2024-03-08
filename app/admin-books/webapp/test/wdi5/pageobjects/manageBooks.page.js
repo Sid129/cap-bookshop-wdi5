02ca1e0d566f1d5accca1ade45aa1b450cd5ff7a
I539940 <daniel.pascual@sap.com>
2024-03-07 23:54:26 -0600

    wdi integration

--- app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -1,9 +1,23 @@
 module.exports = {
-  /**
-    * define actions for the page object
-  */
-
-  /**
-   * define assertions for the page object
-   */
+  iClickOnCreateNewBook: async () => {
+    await browser.asControl({
+    	selector: {
+            id: "books::BooksList--fe::table::Books::LineItem::StandardAction::Create",
+            interaction: {
+                    idSuffix: "BDI-content"
+            }
+      }
+    }).press();
+  },
+  theBookListContains: async (sBook) => {
+    const text = await browser.asControl({
+    	selector: {
+            controlType: "sap.m.Text",
+            viewId: "books::BooksList",
+            properties: {
+                    text: sBook
+            }
+    }}).getText();
+    expect(text).toEqual(sBook);
+  }
 }
---
+++ app/admin-books/webapp/test/wdi5/wdio.conf.js
@@ -0,0 +1,48 @@
+const { TimelineService } = require("wdio-timeline-reporter/timeline-service")
+exports.config = {
+    specs: ["./**/specs/**.js"],
+    // Patterns to exclude.
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+    capabilities: [
+        {
+            maxInstances: 5,
+            browserName: "chrome",
+            "goog:chromeOptions": {
+                args:
+                    // ["--no-sandbox", "--disable-dev-shm-usage"]
+                    process.argv.indexOf("--headless") > -1
+                        ? ["--headless"]
+                        : process.argv.indexOf("--debug") > -1
+                        ? ["window-size=1440,800", "--auto-open-devtools-for-tabs"]
+                        : ["window-size=1440,800"]
+            },
+            acceptInsecureCerts: true
+        }
+    ],
+    logLevel: "error",
+    bail: 0,
+    baseUrl: "http://localhost:4004/fiori-apps.html#Books-manage",
+    waitforTimeout: 60000,
+    connectionRetryTimeout: process.argv.indexOf("--debug") > -1 ? 1200000 : 120000,
+    connectionRetryCount: 3,
+    services: ["ui5", [TimelineService]],
+
+    framework: "mocha",
+    reporters: ["spec", [
+        "timeline",
+        {
+            outputDir: "target",
+            embededImages: true,
+            screenshotStrategy: "before:click",
+        }
+    ]],
+
+    mochaOpts: {
+        ui: "bdd",
+        timeout: process.argv.indexOf("--debug") > -1 ? 600000 : 60000
+    }
+    
+}
